c31d29c87cf64a76ef52cd84180fa871f75b42e5
Bruce Armstrong <bruce@fortressofgeekdom.org>
2017-09-20 16:14:46 -0600

    Waiting updated to handle new libraries

--- js/Waiting.js
+++ js/Waiting.js
@@ -1,15 +1,23 @@
-import Button from 'rebass/dist/Button';
-import Players from './Players';
+import { Card, Text } from './Widgets';
 import Ready from './Ready';
 
 export default class Waiting extends React.Component {
-    render() {
+  player(p) {
     return (
+      <Card key={p.name} bg={p.ready?'green':'red'} color="white">
+        <Text center p={2}>{p.name}</Text>
+      </Card>
+    );
+  }
+
+  render() {
+    const players = this.props.players;
+    const joined = Object.keys(players);
+  return (
 <div>
 	<Ready ready={this.props.me.ready}/>
-    <hr/>
-    <Players players={this.props.players}/>
+    <hr style={{marginTop:"0", marginBottom:"12"}}/>
+    {joined.sort().map((id) => this.player(players[id]))}
 </div>
-    )}
+  )}
 }
-
