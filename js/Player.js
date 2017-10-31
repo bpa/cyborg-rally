b36a4362bdd3bdcb75ced9cdd321571c6acc0e85
Bruce Armstrong <bruce@fortressofgeekdom.org>
2017-10-30 19:22:23 -0600

    Fix #63 - Brakes

--- js/Player.js
+++ js/Player.js
@@ -2,21 +2,29 @@ import { Box, Card, Flex } from './Widgets';
 import Register from './Register';
 import Vitality from "./Vitality";
 import Options from "./Options";
+import Watermark from "./Watermark";
 
-export default class Player extends React.Component {
-    render() {
-        const p = this.props.player;
-        return (
-            <Card bg={p.ready?'green':'red'} color="white" p={1}
-                style={{
-                  display:'flex',
-                  justifyContent:'space-between',
-                  alignItems:'center',
-                }}>
-              <Register register={this.props.register}/>
-              <Options player={p}/>
-              <div style={{padding:'4px 0px'}}>{p.name}{p.shutdown ? '.zZ' : ''}</div>
-              <Vitality player={p}/>
-            </Card>
-    )}
+export default function Player (props) {
+  const p = props.player;
+  let watermark = null;
+  if (props.register && gs.id === props.register.player) {
+    if (p.options.Brakes && props.register.program[0].name === '1') {
+      watermark = <Watermark img='images/brakes.svg' text="Brakes"/>;
+    }
+  }
+
+  return (
+      <Card bg={p.ready?'green':'red'} color="white" p={1}
+          style={{
+            display:'flex',
+            justifyContent:'space-between',
+            alignItems:'center',
+          }}>
+        {watermark}
+        <Register register={props.register}/>
+        <Options player={p}/>
+        <div style={{padding:'4px 0px'}}>{p.name}{p.shutdown ? '.zZ' : ''}</div>
+        <Vitality player={p}/>
+      </Card>
+  )
 }
--- js/Players.js
+++ js/Players.js
@@ -1,13 +1,11 @@
 import Player from './Player';
 
-export default class Players extends React.Component {
-  render() {
-    const players = this.props.players;
-    const alive = Object.keys(players).filter((p)=>!players[p].dead);
-    return (
-    <div>
-      {alive.sort().map((id) => <Player player={players[id]} key={id}/>)}
-    </div>
-    );
-  }
+export default function Players(props) {
+  const players = props.players;
+  const alive = Object.keys(players).filter((p)=>!players[p].dead);
+  return (
+  <div>
+    {alive.sort().map((id) => <Player {...props} player={players[id]} key={id}/>)}
+  </div>
+  );
 }
